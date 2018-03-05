38117b37fcb9420b55dc021cd993e054210b9b87
Mohammad Kafee Uddin <kafee651@users.noreply.github.com>
2018-03-05 21:42:14 +0530

    debug
    debug

--- session.php
+++ session.php
@@ -2,13 +2,16 @@
     session_start();
    
     if( isset( $_SESSION['userid'] ) ) {
+        echo "if";
         header("location: profile.php");
     }
-    else if(!$_POST['userid']{
+    else if($_POST['userid']{
+        echo "elseif";
         $_SESSION['userid']=$_POST['userid'];
         header("location: profile.php");
     }
     else{
+        echo "else";
         header("location: login.php");
     }
       
